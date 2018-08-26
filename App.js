0a76949d41aff325a84b91148b0e423fb1c6ac11
Marcos Barrera del Río <elyomarcos@gmail.com>
2018-08-26 18:36:09 -0500

    Add Toast component to the project.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Text, View, Button, Alert, StyleSheet } from 'react-native';
+import { Text, View, Button, StyleSheet } from 'react-native';
+import Toast from 'react-native-easy-toast'
 
 export default class PizzaTranslator extends Component {
   constructor(props) {
@@ -51,10 +52,10 @@ export default class PizzaTranslator extends Component {
 
     const { questions, index } = this.state;
 
-    if(clikedAnswerIndex === questions[index].answerIndex)
-      Alert.alert('Ajalas, el atinaste :v!');
+    if(clikedAnswerIndex === questions[index].answerIndex) 
+      this.refs.toast.show('Ajalas, le atinaste!');
     else 
-      Alert.alert('Respuesta incorrectas gg');
+      this.refs.toast.show('Respuesta incorrecta');
 
   }
 
@@ -64,7 +65,12 @@ export default class PizzaTranslator extends Component {
 
     return (
       <View style={styles.outerFlexContainer}>
-
+        <Toast 
+          style={styles.toast} 
+          ref="toast"
+          position='bottom'
+          opacity={0.8}
+          />
         <Text style={styles.questionStyle}>
           { questions[index].question }
         </Text>
@@ -103,8 +109,8 @@ const styles = StyleSheet.create({
   button: {margin: '20 0 20 0'},
   innerFlexContainer: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'flex-start',
+    flexDirection: 'column',
+    alignItems: 'center',
     justifyContent: 'space-evenly',
     flexWrap: 'wrap',
     height: 50,
@@ -115,4 +121,5 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     marginTop: 10,
    },
+   toast: {backgroundColor: 'grey', borderRadius: 5,padding: 10,}
 });
